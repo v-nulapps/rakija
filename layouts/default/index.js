ce6a31cd8a6406611d87ca48dd1a01b27f38505b
v-nulapps <123363225+v-nulapps@users.noreply.github.com>
2023-03-01 13:21:45 +0100

    Initialize with-r3f

---
+++ layouts/default/index.js
@@ -0,0 +1,49 @@
+import { Lenis, useLenis } from '@studio-freight/react-lenis'
+import cn from 'clsx'
+import { Cursor } from 'components/cursor'
+import { CustomHead } from 'components/custom-head'
+import { Footer } from 'components/footer'
+import { Header } from 'components/header'
+import { Scrollbar } from 'components/scrollbar'
+import Router from 'next/router'
+import { useEffect } from 'react'
+import s from './layout.module.scss'
+
+export function Layout({
+  seo = { title: '', description: '', image: '', keywords: '' },
+  children,
+  theme = 'light',
+  className,
+}) {
+  const lenis = useLenis()
+
+  useEffect(() => {
+    function onHashChangeStart(url) {
+      url = '#' + url.split('#').pop()
+      lenis.scrollTo(url)
+    }
+
+    Router.events.on('hashChangeStart', onHashChangeStart)
+
+    return () => {
+      Router.events.off('hashChangeStart', onHashChangeStart)
+    }
+  }, [lenis])
+
+  return (
+    <>
+      <CustomHead {...seo} />
+      <Lenis root>
+        <div className={cn(`theme-${theme}`, s.layout, className)}>
+          <div className={s.content}>
+            <Cursor />
+            <Scrollbar />
+            <Header />
+            <main className={s.main}>{children}</main>
+            <Footer />
+          </div>
+        </div>
+      </Lenis>
+    </>
+  )
+}
